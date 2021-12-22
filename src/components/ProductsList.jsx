a98dc901c26758a4c2f6c80ee3a6d58d849e9d5e
Gogi <gogion@protonmail.com>
2021-12-22 21:26:17 +0200

    added prev, next buttons for pagination + made images smaller

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -8,17 +8,22 @@ function ProductsList() {
 
 	const location = useLocation();
 	const { from } = location.state;
+	const [pageNo, setPageNo] = useState(1);
+	const [totalNumberOfProducts, setTotalNumberOfProducts] = useState(0);
 
 	async function getProductsList() {
 		// console.log("category", from);
 		const API = `http://bp-interview.herokuapp.com/categories/${from}/products`;
 		// how to add pagination later:
-		// const API = `http://bp-interview.herokuapp.com/categories/${categoryId}/products?page=1&limit=2`;
+		// const API = `http://bp-interview.herokuapp.com/categories/${from}/products?page=1&limit=2`;
+		// const API = `http://bp-interview.herokuapp.com/categories/${from}/products?page=${pageNo}&limit=2`;
 
 		try {
-			const response = await fetch(API);
+			const firstResponse = await fetch(API);
+			const allData = await firstResponse.json();
+			setTotalNumberOfProducts(allData.length)
+			const response = await fetch(API + `?page=${pageNo}&limit=15`);
 			const data = await response.json();
-			// console.log(data);
 			setProductsList(data);
 		} catch (error) {
 			console.log(error);
@@ -28,7 +33,22 @@ function ProductsList() {
 
 	useEffect(() => {
 		getProductsList();
-	}, []);
+	}, [pageNo]);
+
+	function renderPreviousPage() {
+		console.log("before Prev", pageNo);
+		if (pageNo >= 2) {
+			setPageNo(pageNo - 1);
+		}
+		// console.log("after Prev", pageNo);
+	}
+	function renderNextPage() {
+		console.log("before Next", pageNo);
+		if (pageNo < totalNumberOfProducts) {
+			setPageNo(pageNo + 1);
+		}
+	}
+	console.log("pageNo", pageNo);
 
 	return (
 		<div>
@@ -37,10 +57,24 @@ function ProductsList() {
 			</nav>
 			<h2>A listing page, which will have a list of products.</h2>
 			<div>
+				<input
+					type="range"
+					name="price-range"
+					id="price-range"
+					min="0"
+					max="5000"
+				/>
+			</div>
+			<button onClick={renderPreviousPage}>previous</button>
+			<button onClick={renderNextPage}>next</button>
+			<div>
+				{/* {from.price} */}
 				{productsList.map((product) => (
 					<ProductsListItem key={product.id} item={product} />
-				))}
+					))}
 			</div>
+					<button onClick={renderPreviousPage}>previous</button>
+					<button onClick={renderNextPage}>next</button>
 		</div>
 	);
 }
