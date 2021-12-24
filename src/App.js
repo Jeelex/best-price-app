d4ca559076340b17ac97186fef2ca3e335a009c9
Gogi <gogion@protonmail.com>
2021-12-24 13:32:23 +0200

    installed chakra ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import Home from "./components/Home";
 import ProductsList from "./components/ProductsList";
@@ -7,13 +8,15 @@ import Product from "./components/Product";
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="categories/:id" element={<ProductsList />} />
-			{/* <Route path="categories/:id/:page" element={<ProductsList />} /> */}
-			{/* <Route path="categories/:id?page=1&limit=10" element={<ProductsList />} /> */}
-			<Route path="products/:id" element={<Product />} />
-		</Routes>
+		<ChakraProvider>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="categories/:id" element={<ProductsList />} />
+				{/* <Route path="categories/:id/:page" element={<ProductsList />} /> */}
+				{/* <Route path="categories/:id?page=1&limit=10" element={<ProductsList />} /> */}
+				<Route path="products/:id" element={<Product />} />
+			</Routes>
+		</ChakraProvider>
 	);
 }
 
